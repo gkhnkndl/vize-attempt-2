66839779c4451f04a94c5a94b3688980974ebe3f
gkhnkndl <gokhankundala@gmail.com>
2024-04-21 17:53:27 +0300

    camera and location permissions in setting compeleted

--- vizeapp/lib/screens/settings_page.dart
+++ vizeapp/lib/screens/settings_page.dart
@@ -1,4 +1,8 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 import 'package:vizeapp/theme/theme_provider.dart';
 
@@ -10,13 +14,94 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+  MaterialStateProperty<Color?> overlayColor =
+      MaterialStateColor.resolveWith((states) => Colors.grey);
   bool isSwitched = false;
+
+  String camResult = "";
+  String locationResult = "";
+
+  controlPermission() async {
+    var status = await Permission.camera.status;
+    var statusLocation = await Permission.location.status;
+
+    switch (status) {
+      case (PermissionStatus.granted):
+        setState(() {
+          camResult = "Yetki Onaylandi";
+        });
+        break;
+      case (PermissionStatus.denied):
+        setState(() {
+          camResult = "Yetki Reddedildi";
+        });
+        break;
+      case PermissionStatus.restricted:
+        setState(() {
+          camResult = "Yetki Kisitlandi";
+        });
+        break;
+      case PermissionStatus.limited:
+        setState(() {
+          camResult = "Yetki Limitlendi";
+        });
+        break;
+      case PermissionStatus.permanentlyDenied:
+        setState(() {
+          camResult = "Yetki Kalici Olarak Reddedildi";
+        });
+        break;
+      case PermissionStatus.provisional:
+        setState(() {
+          camResult = "Provisional";
+        });
+        break;
+    }
+    switch (statusLocation) {
+      case (PermissionStatus.granted):
+        setState(() {
+          locationResult = "Yetki Onaylandi";
+        });
+        break;
+      case (PermissionStatus.denied):
+        setState(() {
+          locationResult = "Yetki Reddedildi";
+        });
+        break;
+      case PermissionStatus.restricted:
+        setState(() {
+          locationResult = "Yetki Kisitlandi";
+        });
+        break;
+      case PermissionStatus.limited:
+        setState(() {
+          locationResult = "Yetki Limitlendi";
+        });
+        break;
+      case PermissionStatus.permanentlyDenied:
+        setState(() {
+          locationResult = "Yetki Kalici Olarak Reddedildi";
+        });
+        break;
+      case PermissionStatus.provisional:
+        setState(() {
+          locationResult = "Provisional";
+        });
+        break;
+    }
+  }
+
+  @override
+  void initState() {
+    controlPermission();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:const Text("Settings"),
+        title: const Text("Settings"),
         centerTitle: true,
         backgroundColor: Theme.of(context).colorScheme.background,
       ),
@@ -29,11 +114,11 @@ class _SettingsPageState extends State<SettingsPage> {
                 padding: const EdgeInsets.symmetric(horizontal: 25.0),
                 child: Row(
                   children: [
-                    const Text("Theme: ", style: TextStyle(fontSize: 25)),                    
+                    const Text("Theme: ", style: TextStyle(fontSize: 25)),
                     Switch(
                       activeTrackColor: Theme.of(context).colorScheme.secondary,
-                      activeColor:const Color.fromARGB(255, 218, 221, 211),
-                      value: isSwitched,                                              
+                      activeColor: const Color.fromARGB(255, 218, 221, 211),
+                      value: isSwitched,
                       onChanged: (value) => setState(
                         () {
                           isSwitched = value;
@@ -58,13 +143,43 @@ class _SettingsPageState extends State<SettingsPage> {
                           Provider.of<ThemeProvider>(context, listen: false)
                               .toggleTheme(),
                       color: Theme.of(context).primaryColor,
-                      child:const Text(
+                      child: const Text(
                         "En-Tr",
                         style: TextStyle(fontSize: 25),
                       ),
                     ),
                   ],
                 ),
+              ),
+              const Gap(50),
+              ExpansionTile(
+                title: const Text("Camera Permissions"),
+                children: [
+                  Text(camResult),
+                  const Gap(10),
+                  ElevatedButton(
+                      style: ButtonStyle(backgroundColor: overlayColor),
+                      onPressed: () async {
+                        final status = await Permission.camera.request();
+                      },
+                      child:const Text("Ask for Permission")),
+                  const Gap(10),
+                ],
+              ),
+              const Gap(10),
+              ExpansionTile(
+                title: const Text("Location Permissions"),
+                children: [
+                  Text(locationResult),
+                  const Gap(10),
+                  ElevatedButton(
+                      style: ButtonStyle(backgroundColor: overlayColor),
+                      onPressed: () async {
+                        final status = await Permission.location.request();
+                      },
+                      child:const Text("Ask for Permission")),
+                      const Gap(10),
+                ],
               ),
             ],
           ),
