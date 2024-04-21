65137609c5c3a8d7037da8b3266a954b33815cc9
gkhnkndl <gokhankundala@gmail.com>
2024-04-21 21:19:12 +0300

    empty address page and addressCubit ready for pag e decoration and addAdress funtion

--- vizeapp/lib/screens/adress_page.dart
+++ vizeapp/lib/screens/adress_page.dart
@@ -1,4 +1,6 @@
-import 'package:flutter/material.dart';
+/*import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:vizeapp/bloc/adress/address_cubit.dart';
 
 class AddressPage extends StatefulWidget {
   const AddressPage({super.key});
@@ -8,16 +10,68 @@ class AddressPage extends StatefulWidget {
 }
 
 class _ShopPageState extends State<AddressPage> {
+  late AddressCubit addressCubit;
+
+  @override
+  void initState() {
+    addressCubit = context.read<AddressCubit>();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(     
+    return BlocProvider(
+        create: (context) => AddressCubit(AddressState(address: [])),
+      child: Center(
+        child: Text("Addreslerim" + addressCubit.state.address.toString()),
+      ),
+      );      
+  }
+}
+*/
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:vizeapp/bloc/adress/address_cubit.dart';
+
+class AddressPage extends StatelessWidget {
+  const AddressPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => AddressCubit(AddressState(address: [])),
+      child: AddressPageContent(),
+    );
+  }
+}
+
+class AddressPageContent extends StatefulWidget {
+  @override
+  State<AddressPageContent> createState() => _AddressPageContentState();
+}
+
+class _AddressPageContentState extends State<AddressPageContent> {
+  late AddressCubit addressCubit;
+
+  @override
+  void initState() {
+    addressCubit = context.read<AddressCubit>();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
-        title:const Text("Address"),
         centerTitle: true,
-        backgroundColor: Theme.of(context).colorScheme.background,
-      ), 
-      body:const Center(
-        child: Text("ADDRESS"),
+        title: Text('Addresses'),
+      ),
+      body: Center(
+        child: BlocBuilder<AddressCubit, AddressState>(
+          builder: (context, state) {
+            return Text("Addresses: ${state.address}");
+          },
+        ),
       ),
     );
   }
