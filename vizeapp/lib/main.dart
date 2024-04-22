3ba6a6327a93ee8ec761c41121ba18aef1e6c470
gkhnkndl <gokhankundala@gmail.com>
2024-04-22 20:47:27 +0300

    trying to save settings page

--- vizeapp/lib/main.dart
+++ vizeapp/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:provider/provider.dart';
-import 'package:vizeapp/bloc/adress/address_cubit.dart';
 import 'package:vizeapp/bloc/client/client_cubit.dart';
 import 'package:vizeapp/models/cart.dart';
 
@@ -10,27 +9,27 @@ import '../core/routes.dart';
 import 'theme/theme_provider.dart';
 
 void main() async {
+
   //init the hive
   await Hive.initFlutter();
 
   //open a box
   var box = await Hive.openBox('mybox');
 
-  runApp(ChangeNotifierProvider(
-    create: (context) => ThemeProvider(),
-    child: const MyApp(),
-  ));
+  runApp( ChangeNotifierProvider(create: (context) => ThemeProvider(),
+  child: const MyApp(),));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+ 
 
   @override
   Widget build(BuildContext context) {
     /*daha önce ChangeNotifierProvider kullandım
     bu yüzden BlocProvider kullanmak için MultiProvider kullandım
     https://stackoverflow.com/questions/61052629/how-to-add-multiple-changenotifierprovider-in-same-type-in-flutter
-    */
+    */ 
     return MultiProvider(providers: [
       //Add cart ve cart item için provider
       ChangeNotifierProvider(
